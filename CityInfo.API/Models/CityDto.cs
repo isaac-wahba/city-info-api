c7578b4eff7d46957288e8ef6c26fa04ac6c2376
isaac-wahba <isaac.wahba2511@gmail.com>
2024-06-22 09:52:14 +0300

    add basic endpoints with a coded store for data

---
+++ CityInfo.API/Models/CityDto.cs
@@ -0,0 +1,21 @@
+﻿namespace CityInfo.API.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        // one to many relationship
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
+
+        // calculated field.
+        public int NumberOfPointsOfInterest 
+        { 
+            get 
+            { 
+                return PointsOfInterest.Count;
+            } 
+        }
+    }
+}
